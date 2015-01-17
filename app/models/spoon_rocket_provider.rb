821a7b812fcbcce7b5819098ba9ee31ce63314d2
Max Gordon <gordonmaxc@gmail.com>
2015-01-16 21:40:31 -0800

    RSP active

--- app/models/spoon_rocket_provider.rb
+++ app/models/spoon_rocket_provider.rb
@@ -1,7 +1,7 @@
 class SpoonRocketProvider < SearchProvider
   def average_delivery_time
-    if @address.city == "San Francisco"
       service_info = fetch_data("https://api.spoonrocket.com/userapi/menu?zone_id=2")
+    if @address.city == "San Francisco" && service_info["active"]
       service_info["avg_delivery_time"]
     end
   end
